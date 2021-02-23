e3fbb18da8babb5cd8f3067dfe780d2b255512c3
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-02-23 17:55:09 +0900

    Update g++ arguments

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $1 = "cpp" ]; then
-  g++ source.cpp -o cpp_run.out -DLOCAL_DBG=1
+  g++ source.cpp -o cpp_run.out -std=gnu++17 -Wall -Wextra -DLOCAL_DBG=1
   ./cpp_run.out
 elif [ $1 = "python" ]; then
   python3 source.py
