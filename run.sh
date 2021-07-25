23be5edba4c027fd74325a900ee9da8a7c737a59
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-07-25 13:04:12 +0900

    Update line

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 if [ $1 = "cpp" ]; then
-  g++ ./cpp/source.cpp -o ./cpp/run.out -std=gnu++17 -Wall -Wextra -DLOCAL_DEBUG=1
-  ./cpp/run.out
+  g++ ./cpp/source.cpp -o ./cpp/run.out -std=gnu++17 -Wall -Wextra -DLOCAL_DEBUG=1 && ./cpp/run.out
 elif [ $1 = "python" ]; then
   python3 ./python/source.py
 else
