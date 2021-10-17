b25bca4228ff5ca4d564fa8475d82fae8783ddb4
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-10-17 23:26:18 +0900

    Add TopologicalSort

--- cpp/graph-algorithm.cpp
+++ cpp/graph-algorithm.cpp
@@ -161,3 +161,75 @@ struct FordFulkerson {
     }
 
 };
+
+/**
+ * トポロジカルソート
+ * 与えられた有向グラフをもとに頂点に順序を付ける
+ * 
+ * 一度しか実行できず，結果は is_dag, sorted に格納される
+ * DAG (有向非巡回グラフ，directed acyclic graph) のときは順序が付けられない
+ * 
+ * 複数回実行するには exec, exec_with_priority 内にて _deg をコピーして使えばよい
+ */
+
+struct TopologicalSort {
+    // graph
+    int _n;
+    vector<vll> _es;
+    vll _deg;
+
+    // result of sort
+    bool is_dag;
+    vll sorted;
+
+    TopologicalSort(ll n) : _n(n), _es(n), _deg(n) {}
+
+    // O(1)
+    void add_edge(ll from, ll to){
+        _es[from].emplace_back(to);
+        _deg[to]++;
+    }
+
+    /**
+     * O(n)
+     */
+    void exec() {
+        queue<ll> q;
+        rep (i, _n) {
+            if (_deg[i] == 0) q.emplace(i);
+        }
+        while (!q.empty()) {
+            ll x = q.front();
+            q.pop();
+            sorted.emplace_back(x);
+            each (y : _es[x]) {
+                if (--_deg[y] == 0) q.push(y);
+            }
+        }
+        is_dag = *max_element(_deg.begin(), _deg.end()) == 0;
+    }
+
+    /**
+     * consider the vertex priority with f
+     * 
+     * O(n \log n * f(n))
+     * 
+     * @param f default: greater<ll>; lexical order, O(1)
+     */
+    void exec_with_priority(function<ll(ll, ll)> f = greater<ll>()) {
+        priority_queue<ll, vector<ll>, decltype(f)> q(f);
+        rep (i, _n) {
+            if (_deg[i] == 0) q.emplace(i);
+        }
+        while (!q.empty()) {
+            ll x = q.top();
+            q.pop();
+            sorted.emplace_back(x);
+            each (y : _es[x]) {
+                if (--_deg[y] == 0) q.push(y);
+            }
+        }
+        is_dag = *max_element(_deg.begin(), _deg.end()) == 0;
+    }
+    
+};
