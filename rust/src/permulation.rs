7f03e6a1f2c7ad6ebd8f824edf8f6c8cfdf9edc9
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2022-02-07 23:50:54 +0900

    Add Permulation

---
+++ rust/src/permulation.rs
@@ -0,0 +1,22 @@
+mod template;
+
+struct Permulation { i: usize, p: Vec<usize> }
+impl Permulation {
+    #[allow(unused)]
+    fn new(n: usize) -> Permulation {
+        assert!(0 < n);
+        Permulation { i: 0, p: (0..n).collect() }
+    }
+}
+impl Iterator for Permulation {
+    type Item = (usize, Vec<usize>);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i == INF_U {
+            None
+        } else {
+            let ret = (self.i, self.p.clone());
+            self.i = if self.p.next_permutation() { self.i + 1 } else { INF_U };
+            Some(ret)
+        }
+    }
+}
