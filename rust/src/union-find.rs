644cef88b1401ee3064d089b991b352c8d337bf5
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2022-02-08 18:45:59 +0900

    Add UnionFind

---
+++ rust/src/union-find.rs
@@ -0,0 +1,45 @@
+// UnionFind ======================
+#[allow(unused)]
+impl UnionFind {
+    fn new(n: usize) -> UnionFind {
+        UnionFind { n, root: vec![-1; n] }
+    }
+    
+    fn find(&mut self, x: usize) -> usize {
+        if self.root[x] < 0 {
+            x
+        }
+        else {
+            let ret = self.find(self.root[x] as usize);
+            self.root[x] = ret as isize;
+            ret
+        }
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let mut x = self.find(x);
+        let mut y = self.find(y);
+        if x == y {
+            return
+        }
+        if -self.root[x] > -self.root[y] {
+            swap(&mut x, &mut y);
+        }
+        self.root[y] += self.root[x];
+        self.root[x] = y as isize;
+    }
+
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+
+    fn groups(&mut self) -> HashMap<usize, Vec<usize>> {
+        let mut ret: HashMap<usize, Vec<usize>> = HashMap::new();
+        for i in 0..self.n {
+            let r = self.find(i);
+            ret.entry(r).or_default().push(i);
+        }
+        ret
+    }
+}
+// ================================
