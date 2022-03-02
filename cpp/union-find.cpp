52ae72ec263dba20ad5465b335abc86b95a1fe11
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2022-03-02 22:20:49 +0900

    Add PPUnionFind

--- cpp/union-find.cpp
+++ cpp/union-find.cpp
@@ -1,6 +1,6 @@
 #include "./template.cpp"
 
-// Union-Find =====================
+// Union Find =====================
 struct UnionFind {
 
     int n;
@@ -29,6 +29,38 @@ struct UnionFind {
         umap<int, vi> ret;
         rep (i, n) ret[find(i)].emplace_back(i);
         return ret;
+    }
+
+};
+// ================================
+
+// 部分永続 Union Find =============
+struct PPUnionFind {
+
+    int n;
+    vi root;
+    vi time;
+    int now;
+
+    PPUnionFind(int n) : n(n), root(vi(n, -1)), time(vi(n, 1002003004)), now(0) {}
+
+    int find(int x, int t) {
+        if (t < time[x]) return x;
+        return find(root[x], t);
+    }
+
+    void unite(int x, int y) {
+        now++;
+        x = find(x, now), y = find(y, now);
+        if (x == y) return;
+        if (-root[x] > -root[y]) swap(x, y);
+        root[y] += root[x];
+        root[x] = y;
+        time[x] = now;
+    }
+
+    bool same(int x, int y, int t) {
+        return find(x, t) == find(y, t);
     }
 
 };
